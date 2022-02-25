c86e231823f73b453fdd3a8862b2a7a8435acaf4
Mike <mpech@tuta.io>
2022-02-25 12:18:19 +0200

    .

--- main.c
+++ main.c
@@ -1,12 +1,11 @@
 #include <stdlib.h>
 #include "poly1305.h"
+
 typedef uint8_t u8;
 
 #define ARRAY(name, size) \
     u8 name[size]; \
     for(size_t i = 0; i < size; i++) name[i] = 123;
-
-
 
 int main(void) {
     ARRAY(out, 32);
@@ -17,4 +16,3 @@ int main(void) {
 
 	return 0;
 }
-
